6fd49703080997921881b0c62a183f701c480c3c
sookiemonster <sookiemonster216@gmail.com>
2021-12-02 08:49:29 -0500

    Add Day 2 Pt.2

--- Day 2/day_2.py
+++ Day 2/day_2.py
@@ -1,21 +1,25 @@
 FILENAME = "input.txt"
+HORIZONTAL = 0
+DEPTH = 1
+AIM = 2
 
-def get_move(direction, dist):
+def get_move(direction, dist, aim):
     if ("forward" in direction):
-        return [dist, 0]
+        return [dist, aim * dist, 0]
     elif ("up" in direction):
-        return [0, -1 * dist]
+        return [0, 0, -1 * dist]
     elif ("down" in direction):
-        return [0, dist]
+        return [0, 0, dist]
 
 def main():
-    result = [0, 0]
+    result = [0, 0, 0] # Horizontal, Depth, Aim
     with open(FILENAME) as file_input:
         for line in file_input:
             temp = line.split()
-            move = get_move(temp[0], int(temp[1]))
-            result[0] += move[0]
-            result[1] += move[1]
+            move = get_move(temp[0], int(temp[1]), result[AIM])
+            result[HORIZONTAL] += move[HORIZONTAL]
+            result[DEPTH] += move[DEPTH]
+            result[AIM] += move[AIM]
     print(result[0] * result[1])
 
 main()
